3a098addf388afe133985472801c6523cb1d8368
Dayvid Henrique <Dayvid_159@hotmail.com>
2023-03-03 08:23:49 -0300

    first commit

---
+++ src/main/java/controller/ProjectController.java
@@ -0,0 +1,169 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Project;
+import java.sql.ResultSet;
+import util.ConnectionFactory;
+
+
+/**
+ *
+ * @author USER
+ */
+public class ProjectController {
+
+    public void save(Project project) {
+
+        String sql = "INSERT INTO projects (name, "
+                + "description, "
+                + "createdAt, "
+                + "updatedAt) "
+                + "VALUES (?, ?, ?, ?)";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+
+            //Cria conex�o com o banco    
+            connection = ConnectionFactory.getConnection();
+
+            //Cria um PreparedStatement, classe usada para executar a query
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+
+            //Executa a sql para inser��o dos dados
+            statement.execute();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao salvar o projeto", ex);
+
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+
+    }
+
+    public void update(Project project) throws SQLException {
+
+        String sql = "UPDATE projects SET "
+                + "name = ?, "
+                + "description = ?, "
+                + "createdAt = ?, "
+                + "updatedAt = ? "
+                + "WHERE id = ?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+
+            //Cria��o da conex�o com banco    
+            connection = ConnectionFactory.getConnection();
+
+            //Cria��o da classe PreparedStatement, classe usada para preparar a query
+            statement = connection.prepareStatement(sql);
+
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.setInt(5, project.getId());
+
+            //Execute a sql para inser��o dos dados 
+            statement.execute();
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao atualizar o projeto ", ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+
+        }
+
+    }
+
+    public List<Project> getAll() {
+
+        String sql = "SELECT * FROM projects";
+
+        
+        List<Project> projects = new ArrayList<>();
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        //Classe que vai recuperar os dados do banco de dados
+        ResultSet  resultSet = null;
+
+        try {
+
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            resultSet = statement.executeQuery();
+
+            //Enquanto existir dados no banco de dados
+            while (resultSet.next()) {
+
+                Project project = new Project();
+
+                project.setId(resultSet.getInt("id"));
+                project.setName(resultSet.getString("name"));
+                project.setDescription(resultSet.getString("description"));
+                project.setCreatedAt(resultSet.getDate("createdAt"));
+                project.setUpdatedAt(resultSet.getDate("updatedAt"));
+
+                //Adiciono o contato recuperando, a lista de contatos
+                projects.add(project);
+
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao buscar os projetos ", ex);
+
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement, resultSet);
+
+        }
+        return projects;
+
+    }
+
+    
+    public void removeBYid(int idProject) {
+
+        String sql = "DELETE FROM projects WHERE id = ?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+
+            connection = ConnectionFactory.getConnection();
+
+            statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, idProject);
+
+            statement.execute();
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao deletar o projeto", ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+
+    }
+    
+   }
---
+++ src/main/java/model/Task.java
@@ -0,0 +1,150 @@
+package model;
+
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.swing.text.html.HTML.Tag;
+
+public class Task {
+
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private byte status;
+    private List<Tag> tags;
+    private String notes;
+    private Date deadline;
+    private boolean completed;
+    private Date createdAt;
+    private Date updatedAt;
+    private static final Logger LOG = Logger.getLogger(Task.class.getName());
+
+    public Task() {
+        this.completed = false;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+
+    public Task(String name, String description, byte status, List<Tag> tags, String notes, Date deadline, boolean isCompleted, Date createdAt, Date updatedAt) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.tags = tags;
+        this.notes = notes;
+        this.deadline = deadline;
+        this.completed = isCompleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription(String devem_ser_criadas_telas_para_os_cadastros) {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(byte status) {
+        this.status = status;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public String getNotes(String sem_notas) {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setIsCompleted(boolean b) {
+    }
+
+    public void isIsCompleted(boolean b) {
+    }
+
+
+    public void getDeadline(Date date) {
+    }
+
+    public void getName(String alterar_telas_de_aplicação) {
+    }
+
+    public Object getDescription() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Object isIsCompleted() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
